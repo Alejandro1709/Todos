ada82bc66c33b005eaafe3221d316c7cb7dc1619
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-05 14:12:21 -0500

    Add: Input Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 function App() {
   return (
-    <div className='min-h-screen'>
-      <h1>Hello</h1>
-    </div>
+    <main className='flex justify-center bg-gray-800 min-h-screen'>
+      <section className='md:w-2/5 w-4/5'>
+        <header className='pt-8'>
+          <form className='flex flex-row gap-2 p-2 bg-gray-700 rounded shadow-md'>
+            <input
+              className='bg-transparent flex-1 outline-none placeholder:text-gray-600 text-white'
+              type='text'
+              placeholder='Buy some milk'
+            />
+            <button className='px-2 py-1 text-sm text-white bg-gray-600 rounded hover:bg-gray-700' type='submit'>
+              Add
+            </button>
+          </form>
+        </header>
+      </section>
+    </main>
   );
 }
 
