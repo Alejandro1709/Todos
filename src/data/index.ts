6f1d90abb3afd3c46bcdcb29cd90f92a073d33c0
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-05 15:42:34 -0500

    Add: Dummy Data

---
+++ src/data/index.ts
@@ -0,0 +1,21 @@
+import type Todo from '../types/todo';
+
+const todos: Todo[] = [
+  {
+    id: 1,
+    title: 'Buy some milk',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Finish Physics Assignment',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'Read a book',
+    completed: false,
+  },
+];
+
+export default todos;
