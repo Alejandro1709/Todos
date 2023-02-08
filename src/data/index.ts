34fb2ba25184effc2edd828df7037cea3f1126f9
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-08 14:40:30 -0500

    Remove: Dummy Data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,18 +1,5 @@
 import type Todo from '../types/todo';
 
-const todos: Todo[] = [
-  {
-    id: 1,
-    title: 'Buy some milk',
-  },
-  {
-    id: 2,
-    title: 'Finish Physics Assignment',
-  },
-  {
-    id: 3,
-    title: 'Read a book',
-  },
-];
+const todos: Todo[] = [];
 
 export default todos;
