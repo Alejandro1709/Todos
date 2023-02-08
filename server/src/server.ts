77e902c3d5c7303b4dbcad4f014caea5086e3b0d
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-08 16:38:39 -0500

    Add: MVC Pattern server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,13 @@
 import express from 'express';
-import { initTRPC } from '@trpc/server';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import cors from 'cors';
 import config from './config';
 import morgan from 'morgan';
+import { appRouter } from './routers';
 
 const app = express();
 
 const { PORT, ENV } = config;
-
-const t = initTRPC.create();
-
-const appRouter = t.router({
-  sayHi: t.procedure.query(() => {
-    return 'Hello World';
-  }),
-  logToServer: t.procedure
-    .input((v) => {
-      if (typeof v === 'string') return v;
-
-      throw new Error('invalid input');
-    })
-    .mutation((req) => {
-      console.log(req.input);
-      return true;
-    }),
-});
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
